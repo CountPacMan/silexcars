efdc45e32f20a39e3ee3f33b0e0dc4e924d50bcc
Daniel Toader & John Franti <student@epicodus.com>
2015-03-03 14:52:02 -0800

    clean instantiations in car.php

--- app/app.php
+++ app/app.php
@@ -1,6 +1,5 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/car.php";
 
     $app = new Silex\Application();
 
@@ -33,29 +32,10 @@
     });
 
     $app->get("/results", function() {
-
-        $ferrari = new Car('2012 Ferrari 480', 'images/ferrari.jpeg', 840000, 40000);
-
-        $porsche = new Car('2014 Porsche 911', 'images/porsche.jpg', 114991, 7864);
-
-        $ford = new Car('2011 Ford F450', 'images/ford.jpg', 55995, 14241);
-
-        $lexus = new Car('2013 Lexus RX 350', 'images/lexus.jpg', 44700, 20000);
-
-        $mercedes = new Car('Mercedes Formula One Car', 'images/formulaone.jpg', 1200000, 37979);
-
-        $mercury = new Car('2000 Mercury Sable', 'images/mercury.jpg', 3999, 100000);
-
-        $subaru = new Car('2009 Subaru Outback Sport', 'images/subaru.jpg', 16999, 59600);
-
-        $vw = new Car('2012 Volkswagen GTI', 'images/vw.jpg', 26999, 21500);
-        $cars = array($ferrari, $porsche, $ford, $lexus, $mercedes, $mercury, $subaru, $vw);
-            $cars_matching_search = array();
-            foreach ($cars as $car) {
-                if ($car->worthBuying($_GET['price']) && $car->worthMileage($_GET['miles'])) {
-                    array_push($cars_matching_search, $car);
-                }
-            }
+        $carPrice = $_GET['price'];
+        $carMiles = $_GET['miles'];
+        require_once __DIR__."/../src/car.php";
+        
         $toReturn = "<!DOCTYPE html>
         <html>
         <head>
--- src/car.php
+++ src/car.php
@@ -51,7 +51,31 @@ class Car
     }
 }
 
+$ferrari = new Car('2012 Ferrari 480', 'images/ferrari.jpeg', 840000, 40000);
 
+$porsche = new Car('2014 Porsche 911', 'images/porsche.jpg', 114991, 7864);
+
+$ford = new Car('2011 Ford F450', 'images/ford.jpg', 55995, 14241);
+
+$lexus = new Car('2013 Lexus RX 350', 'images/lexus.jpg', 44700, 20000);
+
+$mercedes = new Car('Mercedes Formula One Car', 'images/formulaone.jpg', 1200000, 37979);
+
+$mercury = new Car('2000 Mercury Sable', 'images/mercury.jpg', 3999, 100000);
+
+$subaru = new Car('2009 Subaru Outback Sport', 'images/subaru.jpg', 16999, 59600);
+
+$vw = new Car('2012 Volkswagen GTI', 'images/vw.jpg', 26999, 21500);
+
+
+
+$cars = array($ferrari, $porsche, $ford, $lexus, $mercedes, $mercury, $subaru, $vw);
+    $cars_matching_search = array();
+    foreach ($cars as $car) {
+        if ($car->worthBuying($carPrice) && $car->worthMileage($carMiles)) {
+            array_push($cars_matching_search, $car);
+        }
+    }
 
 //$ferrari->setPrice("480000.3925");   for test purposes
 
